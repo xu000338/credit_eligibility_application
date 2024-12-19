c249d35cc072e6213c29cd25bf7e41c98b5a2d2c
Swapnil Kangralkar <swapnilforcat@gmail.com>
2024-12-18 21:07:15 -0500

    Create __init__.py

---
+++ data_prep/__init__.py
@@ -0,0 +1,18 @@
+"""
+This init file is not necessary anymore for making a directory a package.
+You can still use it to initialize something and make imports simple
+
+for e.g.
+
+in this file here if you do 
+
+from data_preparation import *
+
+Then while importing the data_prep function elsewhere you won't have to write
+from data_prep.data_preparation import data_prep
+
+instead you can write,
+
+from data_prep import data_pred
+
+"""
